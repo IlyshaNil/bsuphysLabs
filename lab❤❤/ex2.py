75f9fbc30ffd4bc6fc09356ddc8f1a0811f8e7e5
Ilya <hacharoky86@gmail.com>
2021-03-03 15:56:03 +0300

    Update ex2.py

--- lab❤❤/ex2.py
+++ lab❤❤/ex2.py
@@ -8,9 +8,7 @@ x1 = 4.8
 x2 = 6.25
 
 
-def coef(x, y):
-    x.astype(float)
-    y.astype(float)
+def sepDiff(x, y):
     n = len(x)
     a = []
     for i in range(n):
@@ -18,36 +16,27 @@ def coef(x, y):
 
     for j in range(1, n):
         for i in range(n-1, j-1, -1):
-            a[i] = float(a[i]-a[i-1])/float(x[i]-x[i-j])
+            a[i] = float(a[i]-a[i-1]) / float(x[i]-x[i-j])
     return np.array(a) # return an array of coefficient
 
 
-def Eval(a, x, r):
-    x.astype(float)
-    n = len( a ) - 1
+def evalPoly(a, x, r):
+    n = len(a) - 1
     temp = a[n] + (r - x[n])
-    for i in range( n - 1, -1, -1 ):
-        temp = temp * ( r - x[i] ) + a[i]
+    for i in range(n - 1, -1, -1):
+        temp = temp * (r - x[i]) + a[i]
     return temp # return the y_value interpolation
 
 
 def main(x1, x2):
     ynew = []
     xnew = []
-    a = coef(x, y)
     while (x1 <= x2):
-        ynew.append(Eval(a, x, x1))
+        ynew.append(evalPoly(sepDiff(x, y), x, x1))
         xnew.append(x1)
         x1 += 0.01
-    plt.plot(x, y, 'r', xnew, ynew)     #снача строит по точкам из таблицы, "r" - это тип red? потом второй график
+    plt.plot(x, y, 'r', xnew, ynew)    
     plt.grid()
     plt.show()
 
 main(x1, x2)
-
-#print(a)
-#print(ynew)
-
-#plt.plot(x, y, 'r', xnew, ynew)     #снача строит по точкам из таблицы, "r" - это тип red? потом второй график
-#plt.grid()
-#plt.show()
